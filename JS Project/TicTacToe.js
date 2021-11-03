3f14ab08d317c0fb1eb0e7a093a5d65e807600ae
Camden Calquhoun <ccalquhoun19@georgefox.edu>
2021-11-02 18:14:52 -0700

    created board outline

--- JS Project/TicTacToe.js
+++ JS Project/TicTacToe.js
@@ -1,5 +1,14 @@
 function StartGame() {
-    
+
+    $("#cell1");
+    let $cell2 = $("#cell2");
+    let $cell3 = $("#cell3");
+    let $cell4 = $("#cell4");
+    let $cell5 = $("#cell5");
+    let $cell6 = $("#cell6");
+    let $cell7 = $("#cell7");
+    let $cell8 = $("#cell8");
+    let $cell9 = $("#cell9");
 
 }
   
@@ -10,10 +19,3 @@ function addX() {
 function reset() {
     
 }
-
-function change() {
-    $stuff.css({"background-color": "green"});
-}
-
-let $stuff = $("#start-game");
-$stuff.click(change);
