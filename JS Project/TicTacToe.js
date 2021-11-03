745842a528bc1ee1e2a12c000443f894a106dfc0
Camden Calquhoun <ccalquhoun19@georgefox.edu>
2021-11-02 17:23:20 -0700

    testing jquery (submit button color change)

--- JS Project/TicTacToe.js
+++ JS Project/TicTacToe.js
@@ -1,13 +1,19 @@
 function StartGame() {
     
 
-  }
+}
   
-  function addX() {
+function addX() {
 
-  }
+}
   
-  function reset() {
+function reset() {
     
-  }
-  
+}
+
+function change() {
+    $stuff.css({"background-color": "green"});
+}
+
+let $stuff = $("#start-game");
+$stuff.click(change);
