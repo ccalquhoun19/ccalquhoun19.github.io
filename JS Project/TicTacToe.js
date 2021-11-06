3477045f7ed97a66702eddb5a57d946da96d41e7
Camden Calquhoun <ccalquhoun19@georgefox.edu>
2021-11-05 17:25:29 -0700

    add click elements to squares

--- JS Project/TicTacToe.js
+++ JS Project/TicTacToe.js
@@ -18,9 +18,6 @@ let nextIsX = true;
 // counter to determine how many cells are left open
 let boardCount = 0;
 
-// register click handler for board
-// $("");
-
 function StartGame() {
     // display board
     $("#board").css({visibility: "visible"});
@@ -36,13 +33,93 @@ function StartGame() {
     // show whose turn it is -- O starts first
     $("#PlayerO").css("background-color", "blue");
 }
-  
-function addElement() {
-    // when cell is clicked on, add X or O (first should be O)
 
+// 1
+function addElement1() {
+        // first checks if the cell is full
+        if (!$cell1Full) {
+        
+        }
+        
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell1").css("background-color", "red");
+    }
     // check if the cell is already full
 }
-  
+
+// 2
+function addElement2() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell2").css("background-color", "blue");
+    }
+    // check if the cell is already full
+}
+
+// 3
+function addElement3() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell3").css("background-color", "yellow");
+    }
+    // check if the cell is already full
+}
+
+// 4
+function addElement4() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell4").css("background-color", "orange");
+    }
+    // check if the cell is already full
+}
+
+// 5
+function addElement5() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell5").css("background-color", "purple");
+    }
+    // check if the cell is already full
+}
+
+// 6
+function addElement6() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell6").css("background-color", "grey");
+    }
+    // check if the cell is already full
+}
+
+// 7
+function addElement7() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell7").css("background-color", "black");
+    }
+    // check if the cell is already full
+}
+
+// 8
+function addElement8() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell8").css("background-color", "pink");
+    }
+    // check if the cell is already full
+}
+
+// 9
+function addElement9() {
+    // when cell is clicked on, add X or O (first should be O)
+    if (nextIsX) {
+        $("#cell9").css("background-color", "green");
+    }
+    // check if the cell is already full
+}
+
 function reset() {
     // clear the board
 
