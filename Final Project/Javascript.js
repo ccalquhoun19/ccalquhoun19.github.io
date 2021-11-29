200b1d4cdfc0c2f19e763c584357b5a6cf83da7c
Camden Calquhoun <ccalquhoun19@georgefox.edu>
2021-11-28 18:55:53 -0800

    It's working, must save before breaking

--- Final Project/Javascript.js
+++ Final Project/Javascript.js
@@ -1,21 +1,35 @@
 async function sendRequest() {
 
+    // initialize variables for url
     let initialURL = "https://date.nager.at/api/v3/publicholidays/"
-    let userYear = $("#userYear").val();
+    let userYear = $("#user-year").val();
     let userCountry = $("input[name=TypeList]").val();
 
+    // create url for GET request
     let userURL = initialURL + userYear + "/" + userCountry;
 
-    let html = "";
-
+    // fetch the url and check if it's valid
     const response = await fetch(userURL);
     if (response.ok) {
-       const holidays = await response.json();
-       for (let names of holidays) {
-        html += `<li>${names.localName} - ${names.name}</li>`;
-     }
-     document.querySelector("ul").innerHTML = html;
+        // initialize variables for adding elements to the table
+        let dates = "";
+        let localNames = "";
+        let commonNames = "";
+
+        // get response from API
+        const holidays = await response.json();
+
+        // for each date in the response
+        for (let names of holidays) {
+            dates += `<td> ${names.date} </td>`;
+            localNames += `<td> ${names.localName} </td>`;
+            commonNames += `<td> ${names.name} </td>`;
+        }
+        $("#dates-row").html(`<th>Dates</th>`+ dates);
+        $("#local-row").html(`<th>Local Names</th>` + localNames);
+        $("#common-row").html(`<th>Common Names</th>` + commonNames);
     }
+    // display error message if country isn't valid
     else {
        alert("Can't find country. Try another country.");
     }
