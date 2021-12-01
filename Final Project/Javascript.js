6dfb16ddb5cf40e683380d68b62ec3538b9b3971
Camden Calquhoun <ccalquhoun19@georgefox.edu>
2021-11-30 16:05:17 -0800

    got snow background to work

--- Final Project/Javascript.js
+++ Final Project/Javascript.js
@@ -12,6 +12,8 @@ async function sendRequest() {
     let userYear = $("#user-year").val();
     let userCountry = $("input[name=TypeList]").val();
 
+    validateInputs(userYear, userCountry);
+
     // create url for GET request
     let userURL = initialURL + userYear + "/" + userCountry;
 
@@ -28,6 +30,9 @@ async function sendRequest() {
 
         // show table
         $("#holiday-table").css("visibility", "visible");
+
+        // remove height attribute
+        $("#particles-js").css("height", "auto");
 
         // reset table data
         $("#holiday-output").html("<tbody><tr id='country-name'></tr><tr><th id='dates-row'></th><th id='common-row'></th><th id='local-row'></th></tr></tbody>");
@@ -69,6 +74,10 @@ async function sendRequest() {
     }
 }
 
+function validateInputs(year, country) {
+
+}
+
 // if the user hits enter with either field full, search for the holidays
 document.addEventListener("keypress", function(e) {
     if ($("#user-year").val() != "" || $("input[name=TypeList]").val() != "") {
@@ -77,3 +86,117 @@ document.addEventListener("keypress", function(e) {
         }
     }
 });
+
+
+
+// following code retrieved from: https://codepen.io/n-sayenko/pen/qOXKVr
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 400,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 3,
+          "color": "#fff"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 50,
+        "color": "#ffffff",
+        "opacity": 0.6,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 5,
+        "direction": "bottom",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 300,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode":  "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 150,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 200,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.2
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
